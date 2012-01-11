66586163fd1035256952c77daa02472a6e28f063
Rob Yurkowski <rob@godynamo.com>
2012-01-10 23:02:53 -0500

    Adds blockquote method to formatter.

--- lib/forem/formatters/redcarpet.rb
+++ lib/forem/formatters/redcarpet.rb
@@ -7,6 +7,13 @@ module Forem
         formatter = ::Redcarpet::Markdown.new(::Redcarpet::Render::HTML)
         formatter.render(ERB::Util.h(text)).html_safe
       end
+
+
+      def self.blockquote(text)
+        text.split("\n").map do |line|
+          "> " + line
+        end.join("\n")
+      end
     end
   end
 end
